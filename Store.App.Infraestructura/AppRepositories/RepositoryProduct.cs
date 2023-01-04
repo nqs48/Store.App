86a2a5516854263c241e021811ae76cf8edf9afb
Nestea Turner <nqs48@icloud.com>
2023-01-03 21:20:03 -0500

    Creating repository product and implemneting Interface

---
+++ Store.App.Infraestructura/AppRepositories/RepositoryProduct.cs
@@ -0,0 +1,50 @@
+﻿using Store.App.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App.Infraestructura.AppRepositories
+{
+    internal class RepositoryProduct : IRepositoryProduct
+    {
+
+        private readonly AppContext _appContext;
+        
+
+        public RepositoryProduct(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+
+        public Product AddProduct(Product product)
+        {
+            var productAdd= _appContext.Products.Add(product);
+            _appContext.SaveChanges();
+            return productAdd.Entity;
+        }
+
+
+        public void DeleteProduct(int idProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int idProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
