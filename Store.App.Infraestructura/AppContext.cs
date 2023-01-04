533c11788869d2d82114c56ae5bd9e96a6302fff
Nestea Turner <nqs48@icloud.com>
2023-01-03 21:01:08 -0500

    Creating AppContext class implementing DbContext

---
+++ Store.App.Infraestructura/AppContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.App.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App.Infraestructura
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Buy> Buys { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = StoreData");
+            }
+        }
+
+
+    }
+
+
+}
