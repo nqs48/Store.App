ea1d6570c975da1bc552d6730023056d254c0139
Nestea Turner <nqs48@icloud.com>
2023-01-04 13:13:11 -0500

    Creating interface for Buy

---
+++ Store.App.Infraestructura/AppRepositories/IRepositoryBuy.cs
@@ -0,0 +1,16 @@
+﻿using Store.App.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App.Infraestructura.AppRepositories
+{
+    public interface IRepositoryBuy
+    {
+        IEnumerable<Buy> GetAllBuys();
+        Buy AddBuy(Buy buy);
+        Buy GetBuy(int idBuy);
+    }
+}
