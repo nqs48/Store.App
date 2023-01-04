26280bd189e918a3fe5b3e8ab4812a46be7ad1cf
Nestea Turner <nqs48@icloud.com>
2023-01-03 20:28:51 -0500

    Creating Model Product

---
+++ Store.App.Dominio/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App.Dominio
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int InInventory { get; set; }
+        public bool Enabled { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+    }
+}
